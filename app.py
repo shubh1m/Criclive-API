66673c95250e4aa1256a87f85c622681a992bf9a
iamshubh22 <sgshubham03@gmail.com>
2017-02-18 03:21:52 +0530

    Update

--- app.py
+++ app.py
@@ -79,9 +79,7 @@ def main():
     return results
     #else:
     #    return "Invalid command"
-main()
 
-'''if __name__ == '__main__':
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-'''
